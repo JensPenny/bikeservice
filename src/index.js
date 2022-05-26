05806753e1a2da7b36c970654fc96bdea3627c32
jens <jens.penny@gmail.com>
2022-05-26 10:30:27 +0200

    holaworld

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello from root!');
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
